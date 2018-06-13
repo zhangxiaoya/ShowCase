c3ec908a718e6a91b99a88209b8ed09989199cfb
zhangxiaoya <zhangxiaoya1989@gmail.com>
2018-06-13 10:50:05 +0800

    basic demo

---
+++ mainwindow.h
@@ -0,0 +1,40 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+class QAction;
+class QListWidget;
+class QMenu;
+class QTextEdit;
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow();
+
+private slots:
+    void newLetter();
+    void save();
+    void undo();
+    void about();
+    void insertCustomer(const QString &customer);
+    void addParagraph(const QString &paragraph);
+
+private:
+    void createActions();
+    void createStatusBar();
+    void createDockWindows();
+
+    QTextEdit *textEdit;
+    QListWidget *customerList;
+    QListWidget *paragraphsList;
+
+    QMenu *viewMenu;
+};
+
+#endif
